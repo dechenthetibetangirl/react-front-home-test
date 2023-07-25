f3ab92f8f4490d8d1208bcc77dc113e4f9b15bae
Dechen Manckang <dechenwangmoa@gmail.com>
2023-07-25 11:46:07 +0200

    Added/updated images

--- src/Recipes/RecipeData.ts
+++ src/Recipes/RecipeData.ts
@@ -53,9 +53,8 @@ const fondantChocolat: RecipeType = {
   nom: "Fondant au chocolat",
   description:
     "Le fondant au chocolat est un gâteau au chocolat qui fond en bouche",
-  imageURL:
-    "https://www.guydemarle.com/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBc0ZvIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--333bff76e254e1934153f809f2d9cc2b5967da52/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCam9VWTI5dFltbHVaVjl2Y0hScGIyNXpld2c2QzNKbGMybDZaVWtpRFRZd01IZzJNREJlQmpvR1JWUTZER2R5WVhacGRIbEpJZ3REWlc1MFpYSUdPd2RVT2dsamNtOXdTU0lRTmpBd2VEWXdNQ3N3S3pBR093ZFUiLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--929a1958a8630156464b089800ca739f1b4570de/fondant-au-chocolat.jpg",
-  ingredients: [chocolate, egg, flour, butter, sugar],
+    imageURL: "assets/fondantChocolat.jpg",
+    ingredients: [chocolate, egg, flour, butter, sugar],
   etapes: stepsFondantChocolat,
   ingredientsWithQuantity: [
     { ingredient: sugar, quantity: 200, unit: "g" },
@@ -103,7 +102,7 @@ const waldorfSalad: RecipeType = {
 const redFruitTart: RecipeType = {
   nom: "Tarte aux Fruits Rouges",
   description: "Une délicieuse tarte aux fruits rouges, parfaite pour les desserts d'été.",
-  imageURL: "https://example.com/red-fruit-tart-image.jpg",
+  imageURL: "assets/redCrumble.jpeg",
   ingredients: [sugar, butter,redFruits],
   ingredientsWithQuantity: [
     { ingredient: sugar, quantity: 150, unit: "g" },
